0de2e3bcedfc17b33dd030b37e7baee2cc3564a8
Adrian <adrianvalverde45@gmail.com>
2023-08-26 16:40:32 -0400

    user signup through user#create controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
 
-
-    def index
-        
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created 
     end 
+
+    def show
+        render json: @user, status: :ok
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :email, :password, :password_confirmation, :bio)
+    end
 
 end
