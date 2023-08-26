103942eca0de3b202c33c2a8c9d0e15d884b13f8
Adrian <adrianvalverde45@gmail.com>
2023-08-26 18:02:19 -0400

    created navbar

---
+++ client/src/NavBar.js
@@ -0,0 +1,28 @@
+function NavBar() {
+    return (
+        <div>
+            <NavLink>
+                Log In
+            </NavLink>
+        </div>
+    )
+}
+
+// function Navbar() {
+//     return (
+//         <div>
+//             <NavLink to="/CaughtPokemon" exact style={linkStyles} activeStyle={{ background: "darkblue" }}>
+//                 Caught Pokemon
+//             </NavLink>
+//             <NavLink to="/UncaughtPokemon"exact style={linkStyles} activeStyle={{ background: "darkblue" }}>
+//                 Uncaught Pokemon
+//             </NavLink>
+//             <NavLink to="/WildPokemon" exact style={linkStyles} activeStyle={{ background: "darkblue" }}>
+//                 Wild Pokemon
+//             </NavLink>
+//             <NavLink to="/AddPokemon" exact style={linkStyles} activeStyle={{Background: "darkblue" }}>
+//                 Add Pokemon
+//             </NavLink>
+//         </div>
+//     )
+// }
