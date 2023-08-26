5b664360802fcd579e8aa4940065644de52d517d
Adrian <adrianvalverde45@gmail.com>
2023-08-26 18:02:09 -0400

    created client side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home } from "./Home";
+import { CarList } from "./CarList"
+import { NavBar } from "./NavBar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/CarList">
+          <CarList/>
+        </Route>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+      </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
